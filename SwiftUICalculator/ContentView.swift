01096169b53dc572a6b378a109e036c6ea253dba
Ogunsola oluwatimileyn <oyelekeogunsola@gmail.com>
2020-01-06 23:48:28 +0100

    Initial Commit

---
+++ SwiftUICalculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUICalculator
+//
+//  Created by user on 06/01/2020.
+//  Copyright © 2020 user. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
