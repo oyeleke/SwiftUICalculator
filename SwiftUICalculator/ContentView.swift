472704dd9e2693c1e674ae6ab693397263b24296
Ogunsola oluwatimileyn <oyelekeogunsola@gmail.com>
2020-01-07 08:57:57 +0100

    Feat: Added layouts and buttons for the view

--- SwiftUICalculator/ContentView.swift
+++ SwiftUICalculator/ContentView.swift
@@ -9,9 +9,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let button = [
+        ["7", "8", "9", "X"],
+        ["4", "5", "6", "-"],
+        ["1", "2", "3", "+"],
+        ["0", ".", ".", "="]
+    ]
     var body: some View {
-        Text("Hello, World!")
+        ZStack(alignment: .bottom){
+            Color.black.edgesIgnoringSafeArea(.all)
+            HStack{
+                Spacer()
+                Text("42")
+                    .font(.system(size: 64))
+                    .foregroundColor(.white)
+            }.padding()
+            VStack(spacing: 12){
+                ForEach(button, id: \.self) {
+                    row in
+                    HStack (spacing: 12){
+                        ForEach(row, id: \.self) {
+                            button in
+                            Text(button)
+                                .font(.system(size: 32))
+                                .frame(width: self.buttonwidth(), height: self.buttonwidth())
+                                .foregroundColor(.white)
+                                .background(Color.yellow)
+                                .cornerRadius(self.buttonwidth())
+                        }
+                    }
+                }
+            }.padding(.bottom)
+        }
     }
+    
+    func buttonwidth() -> CGFloat {
+        return (UIScreen.main.bounds.width - 5*12) / 4
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
